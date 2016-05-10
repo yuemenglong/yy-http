0954b0176fdeb066d7aa96212cd27bf5e46b776d
yml <yuemenglong@126.com>
2016-05-10 09:56:48 +0800

    add ut of get post json delete

--- test/http-client.test.js
+++ test/http-client.test.js
@@ -1,7 +1,10 @@
 var should = require("should");
 var express = require("express");
+var bodyParser = require('body-parser');
+var qs = require("querystring");
+var Promise = require("bluebird");
 
-var HttpClient = require("..");
+var HttpClient = require("..").HttpClient;
 
 var PORT = 80;
 
@@ -62,5 +65,45 @@ describe('Http Client', function() {
                 done();
             })
         });
+    });
+
+    it('Get Form Json Delete', function(done) {
+        var app = express();
+        app.use(bodyParser.urlencoded({ extended: false }))
+        app.use(bodyParser.json())
+        app.get("/:id", function(req, res) {
+            res.end("GET " + req.params.id);
+        })
+        app.post("/:id", function(req, res) {
+            if (req.is("json")) {
+                res.json(req.body);
+            } else {
+                res.end(qs.stringify(req.body));
+            }
+        })
+        app.delete("/:id", function(req, res) {
+            res.end("DELETE " + req.params.id);
+        })
+        var server = app.listen(PORT);
+        var client = new HttpClient();
+        var obj = { a: 1, b: 2 };
+        Promise.try(function() {
+            return client.get("http://localhost/100");
+        }).then(function(res) {
+            res.data.should.eql("GET 100");
+            return client.form("http://localhost/100", obj);
+        }).then(function(res) {
+            res.data.should.eql(qs.stringify(obj));
+            return client.json("http://localhost/100", obj)
+        }).then(function(res) {
+            res.data.should.eql(JSON.stringify(obj));
+            return client.delete("http://localhost/100");
+        }).then(function(res) {
+            res.data.should.eql("DELETE 100");
+        }).done(function() {
+            server.close(function() {
+                done();
+            })
+        })
     });
 });
